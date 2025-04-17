3f58e02751b7ba1047ea673c36a31bda7370ad34
Rickfortuin <rick.j.fortuin@gmail.com>
2025-04-17 11:50:31 +0200

    Add files via upload

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,13 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import path from './images/Simrig-logo.jpg'
 function Navbar(){
     return (
     <>
       <div className="navbar">
         <nav>
-            <img src={path} />
+            <img src={"./images/Simrig-logo.jpg"} />
             <a class="nav-option" id="home" href="./index.html">Home</a>
             <a class="nav-option" id="builder" href="./builder.html">Builder</a>
             <a class="nav-option" id="products" href="./products.html">Products</a>

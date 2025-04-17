102d4b493236e7a2aafe1bb1142a5f1f011a90a6
Rickfortuin <rick.j.fortuin@gmail.com>
2025-04-17 18:49:57 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Analytics } from "@vercel/analytics/react"
+
 import './index.css'
 import App from './App.jsx'
 import './App.css'
@@ -9,5 +9,5 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-  <Analytics/>
+  
 )
